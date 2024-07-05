65f12449c6382b16ef221b9c4207504a839d6a96
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-05 14:50:29 -0500

    tercer commit se creo logi y registro y ya esta direccionando asu pagina respondiente

--- musica/src/app/app.routes.ts
+++ musica/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import path from 'node:path';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
@@ -8,5 +10,9 @@ export const routes: Routes = [
     },
     {
         path: "login", component: LoginComponent
+    },
+    {
+        path: "registrame", component: RegistroComponent
     }
 ];
+
