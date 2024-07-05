8a8ad2ba6064958bc4a699eb1b79acc142039882
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-05 10:41:54 -0500

    segundo commit se creo la carpeta de login y registro y la instalación del agular y paquete json

--- musica/src/app/app.routes.ts
+++ musica/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: LoginComponent
+    },
+    {
+        path: "login", component: LoginComponent
+    }
+];
